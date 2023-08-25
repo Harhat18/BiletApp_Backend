50262b9113b9be49b1ef3bb025d4e725204d9db4
HarunHatiboglu <harun.hatiboglu@helloworld.com.tr>
2023-08-25 17:17:39 +0300

    Add commit post and delete product

--- Backend/src/controller/products.controller.js
+++ Backend/src/controller/products.controller.js
@@ -4,3 +4,42 @@ export const getProducts = async (req, res) => {
   const product = await Product.find();
   res.status(200).json(product);
 };
+
+export const postProduct = async (req, res) => {
+  const {
+    name,
+    category,
+    description,
+    adress,
+    startDate,
+    endDate,
+    images,
+    map,
+  } = req.body;
+  const product = new Product({
+    name,
+    category,
+    description,
+    adress,
+    startDate,
+    endDate,
+    images,
+    map,
+  });
+  await product.save();
+  res.status(201).json(product);
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const deletedProduct = await Point.findByIdAndDelete(req.params.id);
+    if (!deletedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({ message: "Product deleted", point: deletedProduct });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error deleting Product", error: error.message });
+  }
+};
--- Backend/src/routers/Products.routes.js
+++ Backend/src/routers/Products.routes.js
@@ -1,10 +1,16 @@
 import express from "express";
 
-import { getProducts } from "../controller/products.controller.js";
+import {
+  getProducts,
+  postProduct,
+  deleteProduct,
+} from "../controller/products.controller.js";
 
 const { Router } = express;
 const router = Router();
 
 router.route("/").get(getProducts);
+router.route("/").post(postProduct);
+router.route("/:id").delete(deleteProduct);
 
 export default router;
