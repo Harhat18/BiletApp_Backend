49c7bddd4575e99f57f2040351b0a5c5344017d6
HarunHatiboglu <harun.hatiboglu@helloworld.com.tr>
2023-08-28 09:06:45 +0300

    Add product price

--- src/models/Products.js
+++ src/models/Products.js
@@ -3,6 +3,7 @@ const PoductSchema = new mongoose.Schema({
   title: { type: String, required: true },
   category: { type: String },
   description: { type: String, required: true },
+  price: { type: String, required: true },
   adress: { type: String, required: true },
   date: { type: String, required: true },
   image: { type: String },
